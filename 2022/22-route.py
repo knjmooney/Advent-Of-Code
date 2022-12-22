cd737eef1f580a288148c4b5ead1ef56f303c780
Kevin Mooney <knjmooney@gmail.com>
2022-12-22 17:25:42 +0000

    day 22, part 2

--- 2022/22-route.py
+++ 2022/22-route.py
@@ -1,7 +1,6 @@
 '''
-I modified part 1 to simplify as much as possible until one of the operations
-was a string. At this point it was a matter of solving an algebraic equation,
-it could have been done by hand, but I chose to write a basic solver instead.
+This was a tricky one. I tried to make a generic solution, but in the end,
+I hardcoded the shape.
 '''
 
 from collections import Counter, defaultdict, deque, namedtuple
@@ -27,61 +26,63 @@ data = (
     .decode()
 )
 
-# data = '''\
-#         ...#
-#         .#..
-#         #...
-#         ....
-# ...#.......#
-# ........#...
-# ..#....#....
-# ..........#.
-#         ...#....
-#         .....#..
-#         .#......
-#         ......#.
-
-# 10R5L5R10L4R5L5'''
-
 R=0
 D=1
 L=2
 U=3
-facings = {(0, 1) : R, (1, 0): D, (0, -1) : L, (-1, 0): U,}
+facings = {(0, 1) : R, (1, 0): D, (0, -1) : L, (-1, 0): U}
+ifacings = {v: k for k, v in facings.items()}
+
+N = 50
+flip = lambda x: N - 1 - (x % N)
+asis = lambda x: x % N
+def boundary(p, d):
+    new_p, new_d = {
+        (0, 1): {
+            U: ((3 * N + asis(p[1]), 0 * N), ifacings[R]),
+            L: ((2 * N + flip(p[0]), 0 * N), ifacings[R])
+        },
+        (0, 2): {
+            D: ((1 * N + asis(p[1]), 1 * N + asis(p[0])), ifacings[L]),
+            R: ((2 * N + flip(p[0]), 1 * N + asis(p[1])), ifacings[L]),
+            U: ((3 * N + flip(p[0]), 0 * N + asis(p[1])), ifacings[U])
+        },
+        (1, 1): {
+            R: ((0 * N + asis(p[1]), 2 * N + asis(p[0])), ifacings[U]),
+            L: ((2 * N + asis(p[1]), 0 * N + asis(p[0])), ifacings[D])
+        },
+        (2, 0): {
+            L: ((0 * N + flip(p[0]), 1 * N), ifacings[R]),
+            U: ((1 * N + asis(p[1]), 1 * N + asis(p[0])), ifacings[R])
+        },
+        (2, 1): {
+            R: ((0 * N + flip(p[0]), 2 * N + asis(p[1])), ifacings[L]),
+            D: ((3 * N + asis(p[1]), 0 * N + asis(p[0])), ifacings[L])
+        },
+        (3, 0): {
+            L: ((0 * N, 1 * N + asis(p[0])), ifacings[D]),
+            D: ((0 * N + flip(p[0]), 2 * N + asis(p[1])), ifacings[D]),
+            R: ((2 * N + asis(p[1]), 1 * N + asis(p[0])), ifacings[U])
+        }
+    }[(p[0] // N, p[1] // N)][facings[d]]
+    return new_p, new_d
 
 
-def read_tile_structure(board):
-    # Not sure how to infer this
-    n_tiles_high, n_tiles_wide = (4, 3) if W == 150 else (3, 4)
-    tile_width = W // n_tiles_wide
-    tile_height = H // n_tiles_high
-    assert tile_height == tile_width
-    
-    result = []
-    for i in range(0, H, tile_height):
-        for j in range(0, W, tile_width):
-            if board[(i, j)] != ' ':
-                nns = [(i + x * tile_height, j + y * tile_width) for x, y in facings]
-                neighbours = [
-                    (i // tile_width, j // tile_width) 
-                    if (i, j) in board and board[(i, j)] != ' ' 
-                    else None for i, j in nns
-                ]
-                result.append(neighbours)
-    return result
-
-add = lambda a, b: ((a[0] + b[0]) % H, (a[1] + b[1]) % W)
+add = lambda a, b: ((a[0] + b[0]), (a[1] + b[1]))
 def move(p, d, n):
-    new_p = add(p, d)
-    moved = 0
+    new_p = p
+    new_d = d
+    moved = -1
     while board[new_p] != '#' and moved < n:
-        if board[new_p] == '.':
-            p = new_p
-            moved += 1
-        # else:
-            # assert board[new_p] == ' '
-
+        assert board[new_p] == '.'
+        p = new_p
+        d = new_d
+        moved += 1
         new_p = add(new_p, d)
+        if new_p not in board or board[new_p] == ' ':
+            new_p, new_d = boundary(p, d)
+            assert board[new_p] != ' '
+
     return p, d
 
 
@@ -93,10 +94,10 @@ board = {(i, j): c for i, d in enumerate(data[0]) for j, c in enumerate(d + (' '
 ops = re.findall('L|R|\d+', data[1])
 
 dir = (0, 1)
-op : str
 p = (0, min(i for i, c in enumerate(data[0][0]) if c != ' '))
 
-for op in ops:
+op : str
+for index, op in enumerate(ops):
     if op.isalpha():
         dir = (-dir[1], dir[0]) if op == 'L' else (dir[1], -dir[0])
     else:
@@ -104,6 +105,4 @@ for op in ops:
         p, dir = move(p, dir, int(op))        
 
 facing = facings[dir]
-print(p, facing)
 print(1000 * (p[0] + 1) + 4 * (p[1] + 1) + facing)
-print(read_tile_structure(board))
