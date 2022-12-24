fdda79a95e09199d61cfb76e84ff61b3587a480e
Kevin Mooney <knjmooney@gmail.com>
2022-12-24 11:06:20 +0000

    2022, day 24

--- 2022/24-blizzard.py
+++ 2022/24-blizzard.py
@@ -21,33 +21,6 @@ data = (
     .decode()
 )
 
-# data = '''#.######
-# #>>.<^<#
-# #.<..<<#
-# #>v.><>#
-# #<^v^^>#
-# ######.#'''
-
-# data = '''#.#####
-# #.....#
-# #>....#
-# #.....#
-# #...v.#
-# #.....#
-# #####.#'''
-
-def print_board(board):
-    for i in range(-1, H + 1):
-        for j in range(-1, W + 1):
-            if i == -1 or i == H or j == -1 or j == W:
-                print('#', end='')
-            elif (i, j) not in board:
-                print('.', end='')
-            else:
-                l = board[(i, j)]
-                print(l[0] if len(l) == 1 else len(l), end='')
-        print('')
-
 def next_position(p, c):
     d = {
         '<': (0, -1),
@@ -57,7 +30,6 @@ def next_position(p, c):
     }[c]
     return ((p[0] + d[0]) % H, (p[1] + d[1]) % W)
 
-
 @functools.cache
 def get_board(i):
     if i == 0:
@@ -66,8 +38,6 @@ def get_board(i):
     result = defaultdict(list)
     for p, l in board.items():
         for c in l:
-            np = next_position(p, c)
-            # print(p, np, c)
             result[next_position(p, c)].append(c)     
     return result
 
@@ -82,23 +52,30 @@ H = len(data) - 2
 W = len(data[0]) - 2
 orig_board = {(i-1, j-1): [c] for i, r in enumerate(data) for j, c in enumerate(r) if c in ['>', '<', '^', 'v']}
 
-start = (-1, 0)
-end = (H-1, W-1)
-queue = [(md(start, end), 0, start)]
-seen = set()
+def go(t, start, end):
+    queue = [(t, start)]
+    seen = set()
+    while True:
+        t, p = heappop(queue)
+        if (t, p) in seen:
+            continue
+        seen.add((t, p))
+        board = get_board(t + 1)
+        if p == end:
+            return t
+        for nn in nns(p):
+            if nn == start or nn == end:
+                pass
+            elif nn[0] <= -1 or nn[1] <= -1 or nn[0] >= H or nn[1] >= W:
+                continue
+            elif nn in board:
+                continue
+            heappush(queue, (t + 1, nn))
 
-while True:
-    _, t, p = heappop(queue)
-    if (t, p) in seen:
-        continue
-    seen.add((t, p))
-    board = get_board(t + 1)
-    if p == end:
-        print('end', t + 1)
-        break
-    for nn in nns(p):
-        if (not nn == start) and nn[0] <= -1 or nn[1] <= -1 or nn[0] >= H or nn[1] >= W:
-            continue
-        elif nn in board:
-            continue
-        heappush(queue, (t + md(nn, end), t + 1, nn))
+start = (-1, 0)
+end = (H, W-1)
+
+t = go(0, start, end)
+t = go(t, end, start)
+t = go(t, start, end)
+print(t)
